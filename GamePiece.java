3859353ce81944a218b1e5f088f6265408dbfac1
Saphio <106698837+Saphio@users.noreply.github.com>
2024-05-21 08:36:10 -0700

    Update GamePiece.java

--- GamePiece.java
+++ GamePiece.java
@@ -1,51 +1,95 @@
 package arimaa;
-/**
- * The Game Piece class for Arimaa. 
- */
 
 public class GamePiece {
-
-	////// ATTRIBUTES ///////
-	private int ID;
-	private int side;
-	private String label;
-	private int posX, posY;
-	private boolean alive = true;
-	private int[] turnsAgoPosX = new int[4];
-	private int[] turnsAgoPosY = new int[4];
+	int rank;
 	
-	/////// CONSTRUCTOR ///////
-	public GamePiece (int id, int s, String lbl, int px, int py) {
-		ID = id;
-		side = s;
-		label  = lbl;
-		posX = px;
-		posY = py;
+	//0 is gold, 1 is silver
+	int side;
+	//"Dummy" is for a dummy
+	String label;
+	
+	int column;
+	int row;
+	
+	int[] turnsAgoColumn;
+	
+	int[] turnsAgoRows;
+	
+	
+	boolean isAlive;
+	
+	public GamePiece(int newColumn, int newRow, int newSide, int newRank, String newLabel) {
+		column = newColumn;
+		row = newRow;
+		label = newLabel;
+		isAlive = true;
+		
+		side = newSide;
+		rank = newRank;
 	}
 	
-	/////// METHODS //////
-	// gets ID of the piece
-	public int getID () { return ID; }
-	// gets the side of the piece
-	public int getSide () { return side; }
-	// gets label of the piece
-	public String getLabel () { return label; }
-	// gets position of the piece as an integer array [x, y]
-	public int[] getPos() { return new int[] {posX, posY}; }
-	// returns if the piece is in play
-	public boolean isAlive () { return alive; }
-
-	// shifts the turnsAgoPosX and current position based on a move
-	public void movePiece (int newX, int newY) {
-		for (int i = 2; i >= 0; i--) { 
-			// shifts old moves - 2 shifts to 3, 1 shifts to 2, etc.
-			turnsAgoPosX[i + 1] = turnsAgoPosX[i];
-			turnsAgoPosY[i + 1] = turnsAgoPosY[i];
-		}
-		// updates current position to new position
-		turnsAgoPosX[0] = posX;
-		turnsAgoPosY[0] = posY;
-		posX = newX;
-		posY = newY;
+	public int getRank() {
+		return rank;
 	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getSide() {
+		return side;
+	}
+
+	public void setSide(int side) {
+		this.side = side;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int[] getTurnsAgoColumn() {
+		return turnsAgoColumn;
+	}
+
+	public void setTurnsAgoColumn(int[] turnsAgoColumn) {
+		this.turnsAgoColumn = turnsAgoColumn;
+	}
+
+	public int[] getTurnsAgoRows() {
+		return turnsAgoRows;
+	}
+
+	public void setTurnsAgoRows(int[] turnsAgoRows) {
+		this.turnsAgoRows = turnsAgoRows;
+	}
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+	
 }
