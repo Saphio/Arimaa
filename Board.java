0c44b17bfe17ab18f72fef018e932ec909fbbfa0
Saphio <106698837+Saphio@users.noreply.github.com>
2024-05-21 08:35:41 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,120 +1,121 @@
+//TO DO: Combine the buttons, set up a string to change whenever the button is pressed, and do "if does not equal string, add piece, if does equal string, remove piece"
+//then, after each piece added, run a check to see how many of each piece is on the board and show pieces remaining.
+
 package arimaa;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-import javax.swing.OverlayLayout;
-import javax.swing.SwingConstants;
 
-public class Board extends JFrame implements ActionListener, MouseListener {
+public class Board extends JPanel{
 	
-	/////// SETUP ///////
-	// organization
-	JFrame frame = new JFrame("Arimaa");
-	GameControl game = new GameControl();
-	Container east = new Container();
-	Container south = new Container();
-	Container north = new Container();
+	//Holds the width and height of each square.
+	double width;
+	double height;
+	//color for capture squares
+	public static final Color VERY_LIGHT_RED = new Color(255,102,102);
+	public Board() {
+		super();
+	}
+	//Not sure if we should have this list be here, the main class, or both. Might be better in the main file as more stuff will happen there.
+	//ArrayList<ArrayList<GamePiece>> boardArray = new ArrayList<ArrayList<GamePiece>>();
 	
-	// buttons
-	JButton rabbit = new JButton("Rabbit");
-	JButton cat = new JButton("Cat");
-	JButton dog = new JButton("Dog");
-	JButton horse = new JButton("Horse");
-	JButton camel = new JButton("Camel");
-	JButton elephant = new JButton("Elephant");
-	JButton push = new JButton("Push");
-	JButton pull = new JButton("Pull");
-	JButton endTurn = new JButton("End turn");
-	JButton newGame = new JButton("New game");
+	ArrayList<ArrayList<GamePiece>> boardArray = new ArrayList<ArrayList<GamePiece>>();
 	
-	// labels
-	JLabel curPlayerLbl = new JLabel("Gold's turn", SwingConstants.CENTER);
-	JLabel movesLeftLbl = new JLabel("4 moves left", SwingConstants.CENTER);
-	JLabel addPiecesLbl = new JLabel("Add pieces");
 	
-	/////// CONSTRUCTOR ///////
-	public Board () {
-		// frame
-		frame.setSize(1000, 900);
-		frame.setLayout(new BorderLayout());
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		//width/height of rectangles
+		width = (double)this.getWidth() / 8;
+		height = (double)this.getHeight() / 8;
 		
-		// center
-		game.addMouseListener(this);
-		frame.add(game, BorderLayout.CENTER);
+		g.setColor(Color.LIGHT_GRAY);
 		
-		// east container
-		east.setLayout(new GridLayout(7, 1));
-		east.add(addPiecesLbl);
-		east.add(elephant);
-		east.add(camel);
-		east.add(horse);
-		east.add(dog);
-		east.add(cat);
-		east.add(rabbit);
-		elephant.addActionListener(this);
-		camel.addActionListener(this);
-		horse.addActionListener(this);
-		dog.addActionListener(this);
-		cat.addActionListener(this);
-		rabbit.addActionListener(this);
-		frame.add(east, BorderLayout.EAST);
+		//Draws color for background, and then makes the capture squares red.
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		
-		// south container
-		south.setLayout(new GridLayout(1, 2));
-		south.add(push);
-		south.add(pull);
-		push.addActionListener(this);
-		pull.addActionListener(this);
-		frame.add(south, BorderLayout.SOUTH);
+		g.setColor(VERY_LIGHT_RED);
+		g.fillRect((int)Math.round(width * 2), (int)Math.round(height * 2), (int)width, (int)height);
+		g.fillRect((int)Math.round(width * 5), (int)Math.round(height * 2), (int)width, (int)height);
+		g.fillRect((int)Math.round(width * 2), (int)Math.round(height * 5), (int)width, (int)height);
+		g.fillRect((int)Math.round(width * 5), (int)Math.round(height * 5), (int)width, (int)height);
 		
-		// north container
-		north.setLayout(new GridLayout(2, 2));
-		north.add(endTurn);
-		north.add(newGame);
-		endTurn.addActionListener(this);
-		newGame.addActionListener(this);
-		north.add(curPlayerLbl);
-		north.add(movesLeftLbl);
-		frame.add(north, BorderLayout.NORTH);
+		g.setColor(Color.BLACK);
 		
-		// frame functions
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		//Draws the lines for the board based on the width.
+		for (int x = 0; x < 8 + 1; x++) {
+			//Math.round ensures that the lines are accurate.
+			g.drawLine((int)Math.round(x*width), 0, (int)Math.round(x*width), this.getHeight());
+		} 
+		for (int y = 0; y < 8 + 1; y++) {
+			g.drawLine(0, (int)Math.round(y*height), this.getWidth(), (int)Math.round(y*height));
+		} 
+		/*
+		for (int i = 0; i < boardArray.size(); i++) {
+			if (!boardArray.get(i).equals(null)) {
+				//find the image based on the piece's label
+				Image pieceImage = null;
+				GamePiece currentPiece = boardArray.get(i);
+				try {
+					pieceImage = ImageIO.read(new File(currentPiece.getLabel() + ".png"));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				g.drawImage(pieceImage, (int)((currentPiece.getColumn()) * width), (int)((currentPiece.getRow()) * height), null);
+			}
+		}
+		*/
+		
+		for (int row = 0; row < boardArray.size(); row++) {
+			for (int column = 0; column < boardArray.size(); column++) {
+				if (!boardArray.get(row).get(column).label.equals("Dummy")) {
+					//find the image based on the piece's label
+					Image pieceImage = null;
+					try {
+						pieceImage = ImageIO.read(new File("/home/saphio/" + boardArray.get(row).get(column).getLabel() + ".png"));
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+					g.drawImage(pieceImage, (int)((column) * width), (int)((row) * height), null);
+				}
+			}
+		}
 	}
 	
-	// TODO!
-	// mouse listeners
-	public void mouseReleased(MouseEvent e) {
-		
+	public void addPiece (int column, int row, int side, int rank, String label) {
+		/*
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (i ==row && j == column) {
+					boardArray.get(row).set(column, new GamePiece(column, row, side, rank, label));
+				}
+				else {
+					boardArray.get(i).add(null);
+				}
+			}
+		}
+		*/
+		//boardArray.add(new GamePiece(column, row, side, rank, label));
+		boardArray.get(row).set(column, new GamePiece(column, row, side, rank, label));
 	}
 	
-	// action listeners
-	public void actionPerformed(ActionEvent e) {
-		
-	}
-
-	/////// MAIN ///////
-	public static void main (String[] args) {
-		new Board();
+	public void createBoard() {
+		for (int row = 0; row < 8; row++) {
+			ArrayList<GamePiece> thisRow = new ArrayList<GamePiece>();
+			for (int column = 0; column < 8; column++) {
+				thisRow.add(new GamePiece(column, row, -1, -1, "Dummy"));
+			}
+			boardArray.add(thisRow);
+		}
 	}
 	
-	/////// UNUSED DEFAULT METHODS ///////
-	public void mouseClicked(MouseEvent e) {}
-	public void mousePressed(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
 }
